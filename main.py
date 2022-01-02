fe7c76d2afb3ed42c6692adc466212388bae2681
qPexLegendary <terentev.andrey.2002@gmail.com>
2022-01-02 18:06:20 +0700

    Предусмотрено то, что скрип не найдет информацию о сервере

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ DEFAULT_SERVER_URL = 'http://www.boomlings.com/database'
 URL_LEN = len(DEFAULT_SERVER_URL)
 
 
-def init():
+def init() -> bool:
     global CURRENT_SERVER
     with open(GD_FILE_NAME, mode='rb') as file:
         result = file.read()
         pos = result.find(b'/accounts/loginGJAccount.php')
+
+        if pos == -1:
+            return False
+
         CURRENT_SERVER = result[slice(pos - URL_LEN, pos)]
+        return True
 
 
 def convert(server: bytes):
@@ -33,7 +38,10 @@ def main():
     if not fp.exists(GD_FILE_NAME):
         print(f"Файл {GD_FILE_NAME} не найден.")
         return
-    init()
+
+    if not init():
+        print("Не удается найти информацию о сервере.")
+        return
 
     print(f"URL текущего сервера: {CURRENT_SERVER.decode()}")
     print(f"Введите URL нужного сервера. Пример: {DEFAULT_SERVER_URL}")
@@ -55,3 +63,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    input()
